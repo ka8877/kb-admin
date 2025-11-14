ef2c7547432067976acff1e106a660f4fdb81f26
dmdel <kkang8877@gmail.com>
2025-11-14 16:03:26 +0900

    build: git hub 배포

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -30,7 +30,7 @@ createRoot(document.getElementById('root')!).render(
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <QueryClientProvider client={queryClient}>
-        <BrowserRouter>
+        <BrowserRouter basename={import.meta.env.BASE_URL}>
           <App />
         </BrowserRouter>
         <ToastContainer
